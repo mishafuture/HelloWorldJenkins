05773417d0da36bee42fdbbcbd0e06239834248c
mishafuture <88230054+mishafuture@users.noreply.github.com>
2024-03-06 03:15:51 +0200

    Add files via upload

---
+++ src/main/java/com/example/helloworldjenkins/MainController.java
@@ -0,0 +1,13 @@
+package com.example.helloworldjenkins;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String showPage() {
+        return "main.html";
+    }
+}
